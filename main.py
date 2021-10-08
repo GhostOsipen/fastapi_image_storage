460486eb2c5155f6c8591c5365d8bba73a848ad0
IT1 <gomax94@gmail.com>
2021-10-08 09:33:14 +0300

    wip

--- main.py
+++ main.py
@@ -111,8 +111,6 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
-
-
 #==Routes==
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
